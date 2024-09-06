5d40e24226acda70cfcde856d5efc2a56688f265
Ash Johns <mail@ashpjohns.com>
2024-09-06 17:54:32 -0400

    include recharge in history

--- app/page.tsx
+++ app/page.tsx
@@ -41,6 +41,27 @@ export default function Home() {
     setHistory(h);
   };
 
+  const recharge = () => {
+    setSpent(0);
+    setHistory([...history, 0]);
+  };
+
+  useEffect(() => {
+    setRemaining(budget - spent);
+  }, [budget, spent]);
+
+  useEffect(() => {
+    if (!isReading) {
+      localStorage.setItem(
+        'spent',
+        JSON.stringify({
+          spent,
+          timestamp: new Date().toISOString(),
+        })
+      );
+    }
+  }, [isReading, spent]);
+
   useEffect(() => {
     const storage = localStorage.getItem('spent');
     if (storage) {
@@ -64,22 +85,6 @@ export default function Home() {
     }
   }, []);
 
-  useEffect(() => {
-    setRemaining(budget - spent);
-  }, [budget, spent]);
-
-  useEffect(() => {
-    if (!isReading) {
-      localStorage.setItem(
-        'spent',
-        JSON.stringify({
-          spent,
-          timestamp: new Date().toISOString(),
-        })
-      );
-    }
-  }, [isReading, spent]);
-
   return (
     <main className={styles.main}>
       <h1>Welcome, Ash.</h1>
@@ -94,7 +99,7 @@ export default function Home() {
           </svg>
           Undo
         </button>
-        <button type="button" className={styles.recharge} onClick={() => setSpent(0)} disabled={spent === 0}>
+        <button type="button" className={styles.recharge} onClick={recharge} disabled={spent === 0}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="currentColor">
             <path d="M464 160c8.8 0 16 7.2 16 16l0 160c0 8.8-7.2 16-16 16L80 352c-8.8 0-16-7.2-16-16l0-160c0-8.8 7.2-16 16-16l384 0zM80 96C35.8 96 0 131.8 0 176L0 336c0 44.2 35.8 80 80 80l384 0c44.2 0 80-35.8 80-80l0-16c17.7 0 32-14.3 32-32l0-64c0-17.7-14.3-32-32-32l0-16c0-44.2-35.8-80-80-80L80 96zm368 96L96 192l0 128 352 0 0-128z" />
           </svg>
