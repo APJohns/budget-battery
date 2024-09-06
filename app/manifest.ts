ffaad5d2efb97f07964a62537a976bd580e1e5a1
Ash Johns <mail@ashpjohns.com>
2024-09-06 17:02:19 -0400

    add manifest

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Budget Battery',
+    short_name: 'BudgetBat',
+    description: 'A weekly spending tracker to keep you budget in the green.',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    /* icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ], */
+  };
+}
