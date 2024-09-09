5dcec1331ac9e9245f32d6d7358646c5ac555bd9
Ash Johns <mail@ashpjohns.com>
2024-09-09 18:59:18 -0400

    update theme color

--- app/manifest.ts
+++ app/manifest.ts
@@ -3,12 +3,12 @@ import type { MetadataRoute } from 'next';
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Budget Battery',
-    short_name: 'BudgetBat',
+    short_name: 'Budget Battery',
     description: 'A weekly spending tracker to keep you budget in the green.',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#000000',
+    theme_color: '#34c85a',
     /* icons: [
       {
         src: '/icon-192x192.png',
